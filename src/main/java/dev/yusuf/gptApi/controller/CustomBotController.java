01ce137c0d903251cbc9c6e43271475526b9b938
yusufbgdd557 <yusufbgdd557@gmail.com>
2023-12-28 22:03:49 +0300

    Update Controller

--- src/main/java/dev/yusuf/gptApi/controller/CustomBotController.java
+++ src/main/java/dev/yusuf/gptApi/controller/CustomBotController.java
@@ -4,14 +4,12 @@ import dev.yusuf.gptApi.dto.requests.ChatGPTRequest;
 import dev.yusuf.gptApi.dto.responses.ChatGPTResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/bot")
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/v1")
 public class CustomBotController {
 
     @Value("${openai.model}")
